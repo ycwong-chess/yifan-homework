b67d9b149d19a8fd9a338415788e1a6b574dd611
ycwong-chess <ycwong429@gmail.com>
2020-08-13 02:32:02 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import yifanlogo from '.img/yifan-logo.png';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/*} <header className="App-header">
+        // <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,9 +17,20 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from here! yes!
-        </a>
-      </header>
+          Learn React
+        </a> 
+      </header>*/}
+      <header>
+        <img src={yifanlogo} alt="yifan logo" />
+        <h1 style={{color: "rgb(200,30,200)", fontFamily: "arial", fontSize: 100}}>Welcome to Yifan Homework!</h1>
+          <h2>Yifan Homework is a session where children can learn mandarin after lessons too! We will provide them with after-class homework, audio recordings, practises as well as quizzes!</h2>
+          <h2>It can be categorized into 3 big types</h2>
+            <ul>
+              <li><strong>Audio Recording (课堂录音)</strong></li>
+              <li><strong>One reading Per Week (每周一篇)</strong></li>
+              <li><strong>Quizzes or Games (小测或游戏)</strong></li>
+            </ul>
+      </header> 
     </div>
   );
 }
