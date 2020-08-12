23737363ff34eceb938c55e5cdd41ed5073eef5c
ycwong-chess <ycwong429@gmail.com>
2020-08-13 02:29:17 +0800

    Updates

---
+++ precache-manifest.1bd5e9320b6897875dc3467452ff15e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4073f2a50208ad5057b7182ae34ae34c",
+    "url": "/yifan-homework/index.html"
+  },
+  {
+    "revision": "3a85b573c512de5b0ac7",
+    "url": "/yifan-homework/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c168018b36f9ecae918b",
+    "url": "/yifan-homework/static/js/2.1a084476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yifan-homework/static/js/2.1a084476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a85b573c512de5b0ac7",
+    "url": "/yifan-homework/static/js/main.b613f37d.chunk.js"
+  },
+  {
+    "revision": "e7dd94b3feb430b21e47",
+    "url": "/yifan-homework/static/js/runtime-main.c5ffd070.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yifan-homework/static/media/logo.5d5d9eef.svg"
+  }
+]);
